bbfe534c61aec23d235d38ac3c8694fee4f7fa9b
pyroMechanical <63129694+pyroMechanical@users.noreply.github.com>
2021-04-10 16:31:03 -0700

    added files required for building on other devices

---
+++ build/assets/shaders/compile.bat
@@ -0,0 +1,5 @@
+C:/VulkanSDK/1.2.170.0/Bin32/glslc.exe triangle.vert -o triangle.vert.spv
+C:/VulkanSDK/1.2.170.0/Bin32/glslc.exe triangle.frag -o triangle.frag.spv
+C:/VulkanSDK/1.2.170.0/Bin32/glslc.exe Texture.vert -o Texture.vert.spv
+C:/VulkanSDK/1.2.170.0/Bin32/glslc.exe Texture.frag -o Texture.frag.spv
+pause
